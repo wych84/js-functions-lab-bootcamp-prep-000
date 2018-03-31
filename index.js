9702bab86ecd9acc93e57817b75b1a343cb604a4
wooyangee@gmail.com <wooyangee@gmail.com>
2018-03-31 06:47:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy Holidays!"
+  return 'Happy Holidays!'
 }
 function happyHolidaysTo(name) {
   return 'Happy Holidays, ${name}!'
